c24449df625168d61f967d75f064ca3c89d18eb1
Aphillip22 <amber.rain.phillips@gmail.com>
2021-08-28 19:19:26 -0600

    questions render and store correct answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,8 +59,53 @@ var questions = [
         c: "active",
         answer: "A"
       }
-    ];
-    // end questions
-    function get(x){
-        return document.getElementById(x);
+];
+// end questions
+//get questions
+function get(x){
+    return document.getElementById(x);
+}
+// render a question for display on the page
+function renderQuestion(){
+    test = get("test");
+    if(pos >= questions.length){
+      test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+      get("test_status").innerHTML = "Test completed";
+      // reset var to allow users to restart the test
+      pos = 0;
+      correct = 0;
+      return false;
+    }
+    get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+    
+    question = questions[pos].question;
+    chA = questions[pos].a;
+    chB = questions[pos].b;
+    chC = questions[pos].c;
+    // display the question
+    test.innerHTML = "<h3>"+question+"</h3>";
+    // display the answer selections
+    test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
+    test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
+    test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
+    test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+  }
+  // check user input answer
+  function checkAnswer(){
+    choices = document.getElementsByName("choices");
+    for(var i=0; i<choices.length; i++){
+      if(choices[i].checked){
+        choice = choices[i].value;
       }
+    }
+    // cross check input to correct answer
+    if(choice == questions[pos].answer){
+      // increase score for correct answers
+      correct++;
+    }
+    pos++;
+    // return to renderQuestion
+    renderQuestion();
+  }
+  // Add event listener to call renderQuestion on click
+window.addEventListener("load", renderQuestion);
